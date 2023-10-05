2f47cb142a3b3192e7b3e895abc909d9e76c9f6b
patilk369 <88506217+patilk369@users.noreply.github.com>
2023-10-05 06:51:49 +0000

    comparisond and conversions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1); //t
+// console.log("02" > 1);//t
+
+// console.log(null > 0); // f
+// console.log(null == 0);//f
+// console.log(null >= 0);//t
+
+// console.log(undefined == 0);//f
+// console.log(undefined > 0);//f
+// console.log(undefined < 0);//f
+
+// === 
+
+console.log("2" === 2);//f
+
